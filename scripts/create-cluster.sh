2112db5fd51d6b8c123dda80b12301c367ea250e
Steven Eschinger <nyc_steve@hotmail.com>
2016-09-18 15:46:42 -0400

    Adding full path for kops

--- scripts/create-cluster.sh
+++ scripts/create-cluster.sh
@@ -6,5 +6,5 @@ export KOPS_STATE_STORE=s3://${DOMAIN_NAME}
 echo "export KOPS_STATE_STORE=s3://${DOMAIN_NAME}" >>~/.profile
 
 # Create the cluster
-${GOPATH}/bin/kops create cluster --master-size=${CLUSTER_MASTER_SIZE} --node-size=${CLUSTER_NODE_SIZE} --cloud=aws --zones=${AWS_AVAIL_ZONE} ${DOMAIN_NAME}
-${GOPATH}/bin/kops update cluster ${DOMAIN_NAME} --yes
+/home/vagrant/bin/kops create cluster --master-size=${CLUSTER_MASTER_SIZE} --node-size=${CLUSTER_NODE_SIZE} --cloud=aws --zones=${AWS_AVAIL_ZONE} ${DOMAIN_NAME}
+/home/vagrant/bin/kops update cluster ${DOMAIN_NAME} --yes
--- scripts/delete-cluster.sh
+++ scripts/delete-cluster.sh
@@ -1,5 +1,5 @@
 # Delete the cluster
-kops delete cluster ${DOMAIN_NAME} --yes
+/home/vagrant/bin/kops delete cluster ${DOMAIN_NAME} --yes
 JENKINS_KUBERNETES_VOLUME=$(aws ec2 describe-volumes --filters Name=tag-key,Values="Name" Name=tag-value,Values="jenkins-kubernetes-volume" --output text --query 'Volumes[*].VolumeId')
 export JENKINS_KUBERNETES_VOLUME
 aws ec2 delete-volume --volume-id $JENKINS_KUBERNETES_VOLUME
