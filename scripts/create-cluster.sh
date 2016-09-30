4ce5f40b69341907081a352c22e059a3c283e8dd
Steven Eschinger <nyc_steve@hotmail.com>
2016-09-30 14:46:10 -0400

    Changing Kubernetes cluster version to 1.3.7

--- scripts/create-cluster.sh
+++ scripts/create-cluster.sh
@@ -5,5 +5,5 @@ source ./scripts/variables.sh
 aws s3api create-bucket --region ${AWS_REGION} --bucket ${DOMAIN_NAME}
 
 # Create the Kubernetes cluster
-${GOPATH}/bin/kops create cluster --master-size=${CLUSTER_MASTER_SIZE} --node-size=${CLUSTER_NODE_SIZE} --cloud=aws --zones=${AWS_AVAIL_ZONE} --ssh-public-key="${AWS_KEYPAIR_PUB_KEY_PATH}" ${DOMAIN_NAME}
+${GOPATH}/bin/kops create cluster --master-size=${CLUSTER_MASTER_SIZE} --node-size=${CLUSTER_NODE_SIZE} --cloud=aws --zones=${AWS_AVAIL_ZONE} --ssh-public-key="${AWS_KEYPAIR_PUB_KEY_PATH}" --kubernetes-version=1.3.7 ${DOMAIN_NAME}
 ${GOPATH}/bin/kops update cluster ${DOMAIN_NAME} --yes
