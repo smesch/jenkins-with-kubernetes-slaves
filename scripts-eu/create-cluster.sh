8cc5d3188f4b3c40f0298b9fcebc0a43d6610719
Steven Eschinger <steven@eschinger.com>
2017-03-12 18:14:00 -0400

    Add --context for kubectl / remove region for s3

--- scripts-eu/create-cluster.sh
+++ scripts-eu/create-cluster.sh
@@ -2,7 +2,7 @@
 source ./scripts-eu/variables.sh
 
 # Create the S3 bucket used for Kubernetes cluster configuration storage
-aws s3api create-bucket --region ${AWS_REGION} --bucket ${DOMAIN_NAME}
+aws s3api create-bucket --bucket ${DOMAIN_NAME}
 
 # Create the Kubernetes cluster
 kops create cluster --master-size=${CLUSTER_MASTER_SIZE} --node-size=${CLUSTER_NODE_SIZE} --zones=${AWS_AVAIL_ZONES} --master-zones=${AWS_AVAIL_ZONES} --ssh-public-key="${AWS_KEYPAIR_PUB_KEY_PATH}" --kubernetes-version=1.5.3 ${DOMAIN_NAME}
