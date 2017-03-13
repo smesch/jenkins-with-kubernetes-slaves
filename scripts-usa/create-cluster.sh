1bc174fb8a7ecf5b11420dc0c0f2e72cf994edb2
Steven Eschinger <steven@eschinger.com>
2017-03-12 21:05:58 -0400

    Upgrade from 1.5.3 to 1.6.0-beta.3

--- scripts-usa/create-cluster.sh
+++ scripts-usa/create-cluster.sh
@@ -5,5 +5,5 @@ source ./scripts-usa/variables.sh
 aws s3api create-bucket --bucket ${DOMAIN_NAME}
 
 # Create the Kubernetes cluster
-kops create cluster --master-size=${CLUSTER_MASTER_SIZE} --node-size=${CLUSTER_NODE_SIZE} --zones=${AWS_AVAIL_ZONES} --master-zones=${AWS_AVAIL_ZONES} --ssh-public-key="${AWS_KEYPAIR_PUB_KEY_PATH}" --kubernetes-version=1.5.3 ${DOMAIN_NAME}
+kops create cluster --master-size=${CLUSTER_MASTER_SIZE} --node-size=${CLUSTER_NODE_SIZE} --zones=${AWS_AVAIL_ZONES} --master-zones=${AWS_AVAIL_ZONES} --ssh-public-key="${AWS_KEYPAIR_PUB_KEY_PATH}" --kubernetes-version=1.6.0-beta.3 ${DOMAIN_NAME}
 kops update cluster ${DOMAIN_NAME} --yes
