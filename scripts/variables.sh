66bf407c46902cfefa0ac706262192ad17c7d02b
Steven Eschinger <steven@eschinger.com>
2017-03-15 13:13:17 -0400

    Update cluster values in scripts/variable.sh

--- scripts/variables.sh
+++ scripts/variables.sh
@@ -1,10 +1,10 @@
 # Set values for variables
-export FEDERATION_NAME="k8s-fed"
-export CLUSTER_NAME_USA="k8s-usa"
-export CLUSTER_NAME_EUR="k8s-eur"
+export FEDERATION_NAME="fed"
+export CLUSTER_NAME_USA="usa"
+export CLUSTER_NAME_EUR="eur"
 export TOP_LEVEL_DOMAIN="c3group.io"
-export CLUSTER_CONTEXT_USA="${CLUSTER_NAME_USA}.${TOP_LEVEL_DOMAIN}"
-export CLUSTER_CONTEXT_EUR="${CLUSTER_NAME_EUR}.${TOP_LEVEL_DOMAIN}"
+export CLUSTER_CONTEXT_USA="k8s-${CLUSTER_NAME_USA}.${TOP_LEVEL_DOMAIN}"
+export CLUSTER_CONTEXT_EUR="k8s-${CLUSTER_NAME_EUR}.${TOP_LEVEL_DOMAIN}"
 export JENKINS_DOCKER_IMAGE="smesch/jenkins-kubernetes-leader-custom:2.32.3"
 
 # Set versions for installations
