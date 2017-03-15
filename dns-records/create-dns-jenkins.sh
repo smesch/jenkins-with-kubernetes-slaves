cecc66591c4ead02a1d925947db400ce8b21cbfb
Steven Eschinger <steven@eschinger.com>
2017-03-15 10:43:24 -0400

    Add ELB DNS create scripts

---
+++ dns-records/create-dns-jenkins.sh
@@ -0,0 +1,8 @@
+# Retreive ELB URL
+export JENKINS_ELB=$(kubectl --context=k8s-usa.c3group.io get services/jenkins-leader-svc --template="{{range .status.loadBalancer.ingress}} {{.hostname}} {{end}}")
+
+# Add to JSON file
+sed -i -e "s|JENKINS_ELB|$JENKINS_ELB|g" dns-records/jenkins-leader.json
+
+# Create DNS records
+aws route53 change-resource-record-sets --hosted-zone-id ZUBW5UMTX1U8P --change-batch file://dns-records/jenkins-leader.json
